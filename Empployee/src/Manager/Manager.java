ef846a374fcf9907b24ef36f1062685a4d6e3bb7
王林朋 <15716751907@163.com>
2020-03-08 22:44:53 +0800

    继承修改

--- Empployee/src/Manager/Manager.java
+++ Empployee/src/Manager/Manager.java
@@ -1,18 +1,29 @@
 package Manager;
 import Employee.Employee;
-
+/*
+继承Employee类
+*@param name the employee's name
+*@param s the salary
+*@param year the hire year
+*@param month the hire month
+*param day the hire day
+ */
 public class Manager extends Employee {
-    private double bonus;
-    public Manager(String n, double s, int year, int month, int day) {
-        super(n, s, year, month, day);
-
-
+    private double bonus;  //私有变量
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus=0;
     }
     public void setBonus(double bonus)
     {
         this.bonus=bonus;
-//        return this.bonus;
-        System.out.println(this.bonus);
     }
-
+    /*
+    继承超类getSalary
+     */
+    public double getSalart()
+    {
+        double baseSalary=super.getSalary();  //继承超类需要中的方法,需要super
+        return baseSalary+this.bonus;
+    }
 }
--- Empployee/src/Manager/ManagerTes.java
+++ Empployee/src/Manager/ManagerTes.java
@@ -1,11 +1,22 @@
 package Manager;
 
+import Employee.Employee;
 import Manager.Manager;
 
 public class ManagerTes {
     public  static void main (String[] args){
+            //  construct a Manager object
         Manager bouns=new Manager("name",13,2019,12,23);
         bouns.setBonus(123);
 
+        Employee[] staff=new Employee[3];
+        //以数组方式引用Employee,ManagerTes方法
+        staff[0]=bouns;
+        staff[1]=new Employee("Harry Hacker",500000,1989,10,1);
+        staff[2]=new Employee("Tommy Tester",40000,1990,3,15);
+
+        //输出所有Manager中的对象
+        for(Employee e :staff)  //循环
+           System.out.println("name="+e.getName()+",salary="+e.getSalary());
     }
 }
